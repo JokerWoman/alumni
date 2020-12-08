0073e044e50ffbc6d0e0e12cc5379189034713e3
Andrea Fernandes <andreaisabelfreitas@gmail.com>
2020-12-08 20:10:35 +0000

    Instalei o bootstrap-vue
    + Adicionei nav bar compativel
      com o bootstrap-vue
    
    + Instalei o bootstrap-vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="navbar">
-    <div id="nav">
-      <router-link to="/">Alumni</router-link> |
-      <router-link :to="{ name: 'Eventos' }">Eventos</router-link> |
-      <router-link :to="{ name: 'Bolsas' }">Bolsas de Emprego</router-link> |
-      <router-link :to="{ name: 'Perfil' }">Perfil</router-link> |
-    </div>
-  </div>
+  <!-- o b-nav-item do vue bootstrap comporta-se como um router-link -->
+
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#"></b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{ name: 'Home' }">Alumni</b-nav-item>
+        <b-nav-item :to="{ name: 'Eventos' }">Eventos</b-nav-item>
+        <b-nav-item :to="{ name: 'Bolsas' }">Bolsas de Emprego</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Utilizador" right>
+          <b-dropdown-item :to="{ name: 'Perfil' }">Perfil</b-dropdown-item>
+          <b-dropdown-item href="#">Sair</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
