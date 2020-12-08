ad34b5a9cab7ec4d8538cf272a48aed4ea45c974
Andrea Fernandes <andreaisabelfreitas@gmail.com>
2020-12-08 23:52:46 +0000

    Modifiquei a nav bar e imagem principal
    + Adicionei uma imagem
      principal para o Home.
    
    + Modifiquei o nav bar ao
      estilo proposto no figma.
    
    + Adicionei o genericStyle e
      navBarCustom para o css.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,35 @@
 <template>
   <!-- o b-nav-item do vue bootstrap comporta-se como um router-link -->
 
-  <b-navbar toggleable="lg" type="dark" variant="dark">
+  <b-navbar toggleable="lg" type="light">
     <b-navbar-brand href="#"></b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :to="{ name: 'Home' }">Alumni</b-nav-item>
-        <b-nav-item :to="{ name: 'Eventos' }">Eventos</b-nav-item>
-        <b-nav-item :to="{ name: 'Bolsas' }">Bolsas de Emprego</b-nav-item>
+        <b-nav-item
+          :to="{ name: 'Home' }"
+          :class="{ active: $route.name === 'Home' }"
+          >Alumni</b-nav-item
+        >
+        <b-nav-item
+          :to="{ name: 'Eventos' }"
+          :class="{ active: $route.name === 'Eventos' }"
+          >Eventos</b-nav-item
+        >
+        <b-nav-item
+          :to="{ name: 'Bolsas' }"
+          :class="{ active: $route.name === 'Bolsas' }"
+          >Bolsas de Emprego</b-nav-item
+        >
       </b-navbar-nav>
 
       <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown text="Utilizador" right>
+        <b-nav-item-dropdown
+          text="Utilizador"
+          right
+          :class="{ active: $route.name === 'Perfil' }"
+        >
           <b-dropdown-item :to="{ name: 'Perfil' }">Perfil</b-dropdown-item>
           <b-dropdown-item href="#">Sair</b-dropdown-item>
         </b-nav-item-dropdown>
@@ -27,3 +43,5 @@ export default {
   name: "NavBar"
 };
 </script>
+
+<style src="./../assets/navBarCustom.css"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>Alumni</h1>
+    <b-img right src="@/assets/img/home.png" class="img-responsive w-75">
+    </b-img>
   </div>
 </template>
 
