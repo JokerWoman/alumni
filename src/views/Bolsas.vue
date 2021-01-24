8f43873b6823db4ef23981d5b18378da0c5c93a3
Nuno Silva <40190144@esmad.ipp.pt>
2021-01-24 20:29:29 +0000

    alteração design

--- src/views/Bolsas.vue
+++ src/views/Bolsas.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="bolsas" class="container">
-    
+  <b-container fluid>
     <br />
     <br />
-    <b-row class="justify-content-md-center">
-      <b-col>
+    <b-container fluid>
+    <b-row class="justify-content-md-left">
+      <b-col cols="2"> 
         <p>Data</p>
         <b-form-select v-model="optionSortSelected" :options="optionsSort">
         </b-form-select>
       </b-col>
-      <b-col>
+      <b-col  cols="2" style="margin-top:5px">
         <p>Localidade</p>
         <b-form-input
           type="text"
@@ -17,7 +17,7 @@
           v-model="locality"
         ></b-form-input>
       </b-col>
-      <b-col>
+      <b-col cols="3">
         <p>Tipo</p>
         <b-form-select v-model="categorySelected" :options="getCategories">
           <template #first>
@@ -26,20 +26,22 @@
         </b-form-select>
       </b-col>
     </b-row>
+    </b-container>
     <br />
     <br />
-
-    <b-row>
-      <b-card-group columns v-if="getBolsas.length > 0">
-        <BolsasCard
-          v-for="myBolsa in getBolsas"
-          :key="myBolsa.id"
-          :bolsa="myBolsa"
-        />
-      </b-card-group>
-      <p v-else>Não há ofertas criadas!</p>
-    </b-row>
-  </div>
+    <b-container style="background-color: #2b4c8c;" fluid>
+      <b-row>
+        <b-card-group style="margin:20px" columns v-if="getBolsas.length > 0">
+          <BolsasCard
+            v-for="myBolsa in getBolsas"
+            :key="myBolsa.id"
+            :bolsa="myBolsa"
+          />
+        </b-card-group>
+        <p v-else>Não há ofertas criadas!</p>
+      </b-row>
+    </b-container>
+  </b-container>
 </template>
 
 <script>
@@ -47,7 +49,7 @@ import BolsasCard from "../components/BolsasCard";
 export default {
   name: "Bolsas",
   components: {
-    BolsasCard
+    BolsasCard,
   },
 
   data() {
@@ -58,15 +60,15 @@ export default {
       optionsSort: [
         {
           value: 1,
-          text: "Mais Recentes"
+          text: "Mais Recentes",
         },
         {
           value: -1,
-          text: "Mais Antigos"
-        }
+          text: "Mais Antigos",
+        },
       ],
 
-      filterCategorySelected: "all"
+      filterCategorySelected: "all",
     };
   },
   computed: {
@@ -84,7 +86,7 @@ export default {
     },
     getCategories() {
       return this.$store.getters.getCategoriesForSelect;
-    }
-  }
+    },
+  },
 };
 </script>
