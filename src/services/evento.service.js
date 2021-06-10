4b987b28d5a43739b97c1ee84bb78e7e957f819d
McMarco-ai <macm2000364@gmail.com>
2021-06-10 18:49:24 +0100

    evento.service

--- src/services/evento.service.js
+++ src/services/evento.service.js
@@ -1,12 +1,29 @@
 import API_URL from "./config.js";
 
+import { authHeader } from "./auth.service.js";
+
+
 export const EventoService = {
   async getAllEventos() {
-    var url = new URL(`${API_URL}/evento/`);
-
-    const response = await fetch(url, {
-      method: "GET"
-    });
+    if (user != null) {
+        var params = "";
+        var url = new URL(`${API_URL}/evento`);
+  
+        if (filtros.id_tipoEvento !== "") {
+          params = { id_tipoEvento: filtros.id_tipoEvento };
+        }
+  
+        if (params !== "") {
+          Object.keys(params).forEach(key =>
+            url.searchParams.append(key, params[key])
+          );
+        }
+  
+        const response = await fetch(url, {
+          method: "GET",
+          headers: authHeader(user)
+        });
+   
 
     if (response.ok) {
       let data = await response.json();
@@ -14,70 +31,78 @@ export const EventoService = {
       return data.message;
     } else {
       return null;
+    }
+   }
+  },
+
+  async fetchEventoById(user, id_evento) {
+    if (user != null) {
+      var url = new URL(`${API_URL}/evento/${id_evento}`);
+
+      const response = await fetch(url, {
+        method: "GET",
+        headers: authHeader(user)
+      });
+
+      if (response.ok) {
+        let data = await response.json();
+        console.log(data.message);
+        return data.message;
+      } else {
+        return null;
+      }
+    }
+  },
+  async createEvento(user, evento) {
+    if (user.userType == "professor") {
+      const response = await fetch(`${API_URL}/evento`, {
+        method: "POST",
+        headers: authHeader(user),
+        body: JSON.stringify(evento)
+      });
+
+      let data = await response.json();
+
+      if (!response.ok) {
+        throw Error(data.message);
+      }
     }
   },
 
-  async GetEventoByID(id) {
-    var url = new URL(`${API_URL}/evento/${id}`);
 
-    const response = await fetch(url, {
-      method: "GET"
-    });
-    if (response.ok) {
-      let data = await response.json();
-      console.log(data.message);
-      return data.message;
-    } else {
-      return null;
-    }
-  },
-  async createEvento() {
-    var url = new URL(`${API_URL}/evento/`);
+  async deleteEvento(user, id_evento) {
+    if (user.userType == "professor") {
+      const response = await fetch(`${API_URL}/evento/${id_evento}`, {
+        method: "DELETE",
+        headers: authHeader(user)
+      });
+      if (response.ok) {
+        let data = await response.json();
 
-    const response = await fetch(url, {
-      method: "POST"
-    });
-
-    if (response.ok) {
-      let data = await response.json();
-      console.log(data.message);
-      return data.message;
-    } else {
-      return null;
-    }
-  },
-
-  async deleteEvento() {
-    var url = new URL(`${API_URL}/evento/`);
-
-    const response = await fetch(url, {
-      method: "DELETE"
-    });
-
-    if (response.ok) {
-      let data = await response.json();
-      console.log(data.message);
-      return data.message;
-    } else {
-      return null;
-    }
-  },
-
-  async updateEventoById(id) {
-    var url = new URL(`${API_URL}/evento/${id}`);
-
-    const response = await fetch(url, {
-      method: "PUT"
-    });
-
-    if (response.ok) {
-      let data = await response.json();
-      console.log(data.message);
-      return data.message;
-    } else {
-      return null;
-    }
+        return data.message;
+      } else {
+        return null;
+      }
+    
   }
-};
+},
+
+  async editEvento(user, id_evento, evento) {
+    if (user.userType == "professor") {
+      const response = await fetch(`${API_URL}/evento/${id_evento}`, {
+        method: "PUT",
+        headers: authHeader(user),
+        body: JSON.stringify(evento)
+      });
+
+      let data = await response.json();
+
+      if (!response.ok) {
+        throw Error(data.message);
+      }
+    }
+   }
+  },
+
 
 export default EventoService;
